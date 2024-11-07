271ca78c1bf3f590a063e33b82b70dc14506a8d3
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-07 17:51:06 +0100

    Initial commit

---
+++ app/Models/Type.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    //Aggiungo al modello un metodo che mi restituisca tutti i possibili progetti ad esso connessi
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
+}
